6f6954efaee4ccb9a988c69605507f1065ceed4b
Guillermo García López <memogl25@gmail.com>
2018-10-30 18:55:40 -0600

    delete resources

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ db = DB()
 duolingo_words = db.getAllDuolingoWords()
 
 for duolingo_word in duolingo_words:
-    if len(duolingo_word['word'].split(' ')) == 1 and duolingo_word['word'].find("'") == -1:
+    if len(duolingo_word['word'].split(' ')) == 1 and duolingo_word['word'].find("'") == -1 and duolingo_word['id'] >= 36:
         cambridge_dictionary.getWordData(duolingo_word['word'])
         word_pronunciation = cambridge_dictionary.getPronunciation()
         db.updatePronAndSoundFile(duolingo_word['id'], word_pronunciation)
@@ -16,3 +16,4 @@ for duolingo_word in duolingo_words:
 
 # TODO: aplicar threads in duolingo_words.py para hacerlo mas rapido
 # TODO: utilizar algo para alchemy para la base de datos
+# TODO: utilizar nltk para hacer las busquedas más simples
